a406b351e4bb40236c6beeceedff7b5632604611
Indah <indah.r36@tamu.edu>
2018-02-20 20:04:40 -0600

    canny edge detection

--- IndahRahmadina/edge.py
+++ IndahRahmadina/edge.py
@@ -10,9 +10,17 @@ while(True):
     ret, frame = cap.read()
     
     edges = cv2.Canny(frame,100,200)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #blur= cv2.blur(frame,1) idk how to use blur
     
-    cv2.imshow('frame1',frame)
-    cv2.imshow('frame2',edges)
+    cv2.imshow('original',frame)
+    cv2.imshow('canny',edges)
+    cv2.imshow('grayscale',gray)
+    #cv2.imshow('blur',blur)
+    
+    #plt.imshow(edges,cmap = 'gray')
+    #plt.xticks([]), plt.yticks([]) 
+    #plt.show([])
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
