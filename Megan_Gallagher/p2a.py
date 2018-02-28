4ee001f2b855706d95bf44d4f1f00ebe96a4b23d
Megan Gallagher <mgallagher202@gmail.com>
2018-02-27 20:17:46 -0600

    detection stuff

--- Megan_Gallagher/p2a.py
+++ Megan_Gallagher/p2a.py
@@ -2,7 +2,7 @@ import serial, time
 arduino = serial.Serial('COM4', 115200, timeout=.1)
 time.sleep(1) #give the connection a second to settle
 
-arduino.write("Hello from Python!".encode('utf-8'))
+arduino.write("Hell0 from t!".encode('utf-8'))
 time.sleep(2)
 while True:
 	data = arduino.read()
