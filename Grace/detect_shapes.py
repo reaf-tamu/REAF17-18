f8102d42d7b43e0a85d5ae6f33613cb666a7e857
gracewes <gracewes@tamu.edu>
2018-03-21 14:08:45 -0500

    counting pips on a die based on contour hierarchies

---
+++ Grace/detect_shapes.py
@@ -0,0 +1,51 @@
+from ShapeDetector import ShapeDetector
+import imutils
+import cv2
+
+
+IMAGE = "dice.jpg"
+
+image = cv2.imread(IMAGE)
+resized = imutils.resize(image, width=300)
+ratio = image.shape[0] / float(resized.shape[0])
+
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 50, cv2.THRESH_BINARY)[1]
+
+#cv2.imshow("Threshold", thresh)
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+hierarchy = cnts[-1]
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+sd = ShapeDetector()
+
+print("contours")
+print (cnts)
+print("hierarchy")
+hierarchy = hierarchy[0]
+print(hierarchy[1][0])
+# loop over the contours
+i = 0
+for c in cnts:
+    if(hierarchy[i][3] == -1):
+        # compute the center of the contour, then detect the name of the
+        # shape using only the contour
+        M = cv2.moments(c)
+        cX = int((M["m10"] / M["m00"]) * ratio)
+        cY = int((M["m01"] / M["m00"]) * ratio)
+        shape = sd.detect(c)
+
+        numPips = hierarchy[i][0] - i - 1
+        # multiply the contour (x, y)-coordinates by the resize ratio,
+        # then draw the contours and the name of the shape on the image
+        if(numPips > 0):
+            c = c.astype("float")
+            c *= ratio
+            c = c.astype("int")
+            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+            cv2.putText(image, str(numPips), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # show the output image
+    i += 1
+cv2.imshow("Image", image)
+cv2.waitKey(0)
--- Grace/findCircle.py
+++ Grace/findCircle.py
@@ -5,28 +5,39 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-fgbg = cv2.createBackgroundSubtractorMOG2()
+#fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while True:
     ret, frame = cap.read()
 
-    bkgd = fgbg.apply(frame)
+    thresh = 0
+    maxValue = 255
 
-    blur = cv2.GaussianBlur(bkgd, (5,5), 0, 0)
+    th, dst = cv2.threshold(frame, thresh, maxValue, cv2.THRESH_BINARY)
 
-    edges = cv2.Canny(blur, 100, 200)  
+    #bkgd = fgbg.apply(frame)
+
+    #blur2 = cv2.medianBlur(frame, 3)
+
+    #blur1 = cv2.GaussianBlur(frame, (31,31), 1, 1)
+
+    #edges = cv2.Canny(blur1, 100, 200)
+
+    cv2.imshow('threshhold', th)
     
-    cv2.imshow('Edges', edges)
+    #cv2.imshow('gaussian blur', blur1)
 
-    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1.2, 2000)
+    #cv2.imshow('Gauss Blur', edges)
+
+    '''circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1.2, 2000)
 
     if circles is not None:
         circles = np.round(circles[0,:]).astype("int")
         for(x, y, r) in circles:
             cv2.circle(frame, (x,y), r, (0,255,0), 4)
             cv2.rectangle(frame, (x-5, y-5), (x+5, y+5),(0, 128, 255), -1)
-
-    cv2.imshow('Original', frame)
+    '''
+    #cv2.imshow('Original', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
